6b379beab7ff19b2fc5fa97b2f658fc0ef79f422
Nico <lawani.gk@gmail.com>
2023-08-20 21:40:41 +0100

    add docstrings

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -14,6 +14,7 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initialise dataset"""
         self.__dataset = None
         self.__indexed_dataset = None
 
@@ -40,6 +41,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Get deletion proof paginatated data"""
         dataset = self.indexed_dataset()
         assert index > 0 and index < len(dataset)
         data = []
