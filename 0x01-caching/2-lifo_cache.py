de52a57e215a79bcef698d7a3adad39cc6250a80
Nico <lawani.gk@gmail.com>
2023-08-21 10:40:22 +0100

    refactor lifo implementation

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -6,33 +6,26 @@ from base_caching import BaseCaching
 class LIFOCache(BaseCaching):
     """LIFO caching"""
 
+    def __init__(self):
+        super().__init__()
+        self.stack = []
+
     def put(self, key, item):
         """adds new item to cache"""
-        if not key or not item:
+        if key is None or item is None:
             return
-        if (len(self.cache_data) >= self.MAX_ITEMS
-                and key not in self.cache_data):
-            key_to_discard = list(self.cache_data.keys())[-1]
+        # check if current key exists
+        if key in self.cache_data:
+            self.stack.remove(key)
+        # if key not exists, and current cache is full
+        # Discard the most recent item
+        elif len(self.cache_data) >= self.MAX_ITEMS:
+            key_to_discard = self.stack.pop()
             print(f'DISCARD: {key_to_discard}')
             self.cache_data.pop(key_to_discard)
+        self.stack.append(key)
         self.cache_data[key] = item
 
     def get(self, key):
         """retrieves item from cache"""
         return self.cache_data.get(key)
-
-
-my_cache = LIFOCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-my_cache.put("C", "Street")
-my_cache.print_cache()
-my_cache.put("F", "Mission")
-my_cache.print_cache()
-my_cache.put("G", "San Francisco")
-my_cache.print_cache()
