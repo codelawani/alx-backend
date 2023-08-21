2e88e78a92ed14ca41464130c71ce1489c985d4a
Nico <lawani.gk@gmail.com>
2023-08-21 17:03:43 +0100

    implement redis client with async ops

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,25 @@
+import { createClient } from "redis";
+import redis from "redis"
+import { promisify } from "util"
+const client = createClient();
+client
+  .on('error', (err) => {
+    console.error('Redis client not connected to the server:', err.message)})
+  .on('connect', () => {
+    console.log('Redis client connected to the server')})
+const asyncGet = promisify(client.get).bind(client)
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, (err, reply) => {
+    redis.print(`Reply: ${reply}`)
+  })
+}
+async function displaySchoolValue(schoolName) {
+    const reply = await asyncGet(schoolName)
+    console.log(reply)
+}
+(async () => {
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+
+})()
