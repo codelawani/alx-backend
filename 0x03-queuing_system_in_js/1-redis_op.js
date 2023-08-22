038bd3960c8a2c324556f4033d6fca10b5c15b8f
Nico <lawani.gk@gmail.com>
2023-08-22 09:34:24 +0100

    update eslint and lint scripts

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,21 +1,23 @@
 import { createClient } from "redis";
-import redis from "redis"
+import redis from "redis";
 const client = createClient();
 client
-  .on('error', (err) => {
-    console.error('Redis client not connected to the server:', err.message)})
-  .on('connect', () => {
-    console.log('Redis client connected to the server')})
+  .on("error", (err) => {
+    console.error("Redis client not connected to the server:", err.message);
+  })
+  .on("connect", () => {
+    console.log("Redis client connected to the server");
+  });
 function setNewSchool(schoolName, value) {
   client.set(schoolName, value, (err, reply) => {
-    redis.print(`Reply ${reply}`)
-  })
+    redis.print(`Reply ${reply}`);
+  });
 }
 function displaySchoolValue(schoolName) {
   client.get(schoolName, (err, reply) => {
-    console.log(reply)
-  })
+    console.log(reply);
+  });
 }
-displaySchoolValue('Holberton');
-setNewSchool('HolbertonSanFrancisco', '100');
-displaySchoolValue('HolbertonSanFrancisco');
+displaySchoolValue("Holberton");
+setNewSchool("HolbertonSanFrancisco", "100");
+displaySchoolValue("HolbertonSanFrancisco");
